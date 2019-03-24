28763ebc42661559523bcc37fe1426a0b20de3d2
MicuEmerson <micuemerson@gmail.com>
2019-03-24 19:22:05 +0200

    added some tests (from EC), some of them fail but is okay for first run

--- src/test/java/Repository/MemoryRepository/StudentRepoTest.java
+++ src/test/java/Repository/MemoryRepository/StudentRepoTest.java
@@ -44,8 +44,100 @@ public class StudentRepoTest {
         } catch (ValidatorException e) {
             assert true;
         }
-
     }
 
+    @Test
+    public void addStudentNameEmpty() {
+        try {
+            repo.save(new Student("1", "", 935, "sebinechita@yahoo.com", "Prof. Laura Diosan"));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
 
+    //TODO addStudentId...
+
+    @Test
+    public void addStudentEmail() {
+        try {
+            repo.save(new Student("1", "emi", 935, "abc@gmail.com", "Prof. Laura Diosan"));
+            assert true;
+        } catch (ValidatorException e) {
+            assert false;
+        }
+        assertEquals(repo.size(), 1);
+    }
+
+    @Test
+    public void addStudentEmailNull() {
+        try {
+            repo.save(new Student("1", "emi", 935, null, "Prof. Laura Diosan"));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void addStudentEmailEmpty() {
+        try {
+            repo.save(new Student("1", "emi", 935, "", "Prof. Laura Diosan"));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void addStudentEmailWrong() {
+        try {
+            repo.save(new Student("1", "emi", 935, "abc.com", "Prof. Laura Diosan"));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void addStudentGroup() {
+        try {
+            repo.save(new Student("1", "emi", 935, "abc@gmail.com", "Prof. Laura Diosan"));
+            assert true;
+        } catch (ValidatorException e) {
+            assert false;
+        }
+        assertEquals(repo.size(), 1);
+    }
+
+    @Test
+    public void addStudentGroupWrong() {
+        try {
+            repo.save(new Student("1", "emi", -123, "abc@gmail.com", "Prof. Laura Diosan"));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void addStudentIndrumator() {
+        try {
+            repo.save(new Student("1", "emi", 935, "abc@gmail.com", "Prof. Laura Diosan"));
+            assert true;
+        } catch (ValidatorException e) {
+            assert false;
+        }
+        assertEquals(repo.size(), 1);
+    }
+
+    @Test
+    public void addStudentIndrumatorEmpty() {
+        try {
+            repo.save(new Student("1", "emi", 935, "abc@gmail.com", ""));
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
 }
