7de8b35817b2ca785450123b56e89cd70e9d14af
Sebastian Nechita <sebinechita@yahoo.com>
2019-03-25 13:41:10 +0200

    Fixed source code

--- src/main/java/Validator/StudentValidator.java
+++ src/main/java/Validator/StudentValidator.java
@@ -10,17 +10,21 @@ public class StudentValidator implements IValidator<Student> {
             //throw new ValidatorException("Id invalid\n");
             errors+="Id invalid\n";
         }
-        if(s.getNume().equals("") || s.getNume()==null){
+        if(s.getNume()==null || s.getNume().equals("")){
             //throw new ValidatorException("Nume invalid\n");
             errors+="Nume invalid\n";
         }
-        if(s.getGrupa()<=0){
+        if(s.getGrupa()<0){
             //throw new ValidatorException("Grupa invalida\n");
             errors+="Grupa invalid\n";
         }
-        if(s.getEmail().equals("") || s.getEmail()==null){
+        if(s.getEmail()==null || s.getEmail().equals("") || !s.getEmail().contains("@")){
             //throw new ValidatorException("Email invalid\n");
             errors+="Email invalid\n";
+        }
+        if(s.getIndrumator()==null || s.getIndrumator().equals("")){
+            //throw new ValidatorException("Email invalid\n");
+            errors+="Indrumator invalid\n";
         }
         if (errors.length()!=0){
             throw  new ValidatorException(errors);
